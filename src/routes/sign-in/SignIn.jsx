1cb0df6e27283d9ecdee8ab3ba03e8a86331eb76
unknown <vipulr6111@gmail.com>
2023-02-10 00:22:43 +0530

    storing user necessary data to firestore

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./sign-in.styles.scss";
+import { createUserDocFromAuth, signInWithGooglePopup } from "../../utils/firebase/fireabase";
 
 const SignIn = () => {
-  return <div></div>;
+  const logGoogleUser = async () => {
+    const res = await signInWithGooglePopup();
+    console.log(res);
+    await createUserDocFromAuth(res.user)
+  };
+
+  return (
+    <div>
+      <button onClick={logGoogleUser}>Sign In With Google</button>
+    </div>
+  );
 };
 
 export default SignIn;
- 
--- src/utils/firebase/fireabase.js
+++ src/utils/firebase/fireabase.js
@@ -1,28 +1,60 @@
 import { initializeApp } from "firebase/app";
 
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+/* Collection name for Firestore */
+const COLLECTION_USER = "users";
+
+/*Firebase configuration */
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: "veer-clothing.firebaseapp.com",
-  projectId: "veer-clothing",
-  storageBucket: "veer-clothing.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+	authDomain: "veer-clothing.firebaseapp.com",
+	projectId: "veer-clothing",
+	storageBucket: "veer-clothing.appspot.com",
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
+/*Selecting account from pop-up */
 provider.setCustomParameters({
-  prompt: "select_account",
+	prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider); //signin with pop up display
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, COLLECTION_USER, userAuth.uid); //doc ref for logged in user 
+	const userSnapshot = await getDoc(userDocRef);
+	
+	if (!userSnapshot.exists()) { //if data does not exists in firestore
+		const { displayName, email, photoURL } = userAuth; //this is the info which we need at this time
+		const createdAt = new Date();
+
+		try {
+			/*adding data to firestore */
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				photoURL,
+				createdAt,
+			});
+		} catch (err) {
+			console.log("error creating user", err.message);
+		}
+	}
+
+	return userDocRef;
+};
